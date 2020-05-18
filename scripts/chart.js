0e717e0327240be62d660ec0955ba0ea91bf7019
norm-AU <storminorman772@gmail.com>
2020-05-18 18:51:01 -0500

    chart v1 added

--- scripts/chart.js
+++ scripts/chart.js
@@ -46,16 +46,74 @@ console.log("Date element",slices[0].values[0].date);
 // returns the array's length
 console.log("Array length",(slices[0].values).length);
 //----------------------------SCALES-----------------------------//
+const xScale = d3.scaleTime().range([0,width]);
+const yScale = d3.scaleLinear().rangeRound([height, 0]);
+xScale.domain(d3.extent(data, function(d){
+    return timeConv(d.date)}));
+yScale.domain([(0), d3.max(slices, function(c) {
+    return d3.max(c.values, function(d) {
+        return d.measurement + 4; });
+        })
+    ]);
 
 //-----------------------------AXES------------------------------//
+const yaxis = d3.axisLeft()
+    .ticks((slices[0].values).length)
+    .scale(yScale);
+   
+const xaxis = d3.axisBottom()
+    .ticks(d3.timeDay.every(1))
+    .tickFormat(d3.timeFormat('%b %d'))
+    .scale(xScale);
 
 //----------------------------LINES------------------------------//
-   
+const line = d3.line()
+    .x(function(d) { return xScale(d.date); })
+    .y(function(d) { return yScale(d.measurement); });
+
+let id = 0;
+const ids = function () {
+    return "line-"+id++;
+}
 //-------------------------2. DRAWING----------------------------//
 
 //-----------------------------AXES------------------------------//
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xaxis);
+   
+svg.append("g")
+    .attr("class", "axis")
+    .call(yaxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("dy", ".75em")
+    .attr("y", 6)
+    .style("text-anchor", "end")
+    .text("Frequency");
 
 //----------------------------LINES------------------------------//
+const lines = svg.selectAll("lines")
+    .data(slices)
+    .enter()
+    .append("g");
+   
+lines.append("path")
+    .attr("class", ids)
+    .attr("d", function(d) { return line(d.values); });
 
+lines.append("text")
+    .attr("class","serie_label")
+    .datum(function(d) {
+        return {
+            id: d.id,
+            value: d.values[d.values.length - 1]}; })
+    .attr("transform", function(d) {
+            return "translate(" + (xScale(d.value.date) + 10)  
+            + "," + (yScale(d.value.measurement) + 5 ) + ")";})
+    .attr("x", 5)
+    .text(function(d) { return ("Serie ") + d.id; });
 
 });
+
